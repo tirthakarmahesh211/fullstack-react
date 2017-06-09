144caee74b9d47d943bfc9ad4a81524324b349b3
Alex Griffis <alexgriffis@gmail.com>
2017-06-09 12:59:40 -0700

    Chapter 2: Build Static Timers App

--- fullstack-react-code/time_tracking_app/public/js/app.js
+++ fullstack-react-code/time_tracking_app/public/js/app.js
@@ -1,1 +1,137 @@
+/* global React, helpers, ReactDOM */
+class TimersDashboard extends React.Component {
+  render () {
+    return (
+      <div className='ui three column centered grid'>
+        <div className='column'>
+          <EditableTimerList />
+          <ToggleableTimerForm
+            isOpen
+          />
+        </div>
+      </div>
+    )
+  }
+}
 
+class EditableTimerList extends React.Component {
+  render () {
+    return (
+      <div id='timers'>
+        <EditableTimer
+          title='Learn React'
+          project='Web Domination'
+          elapsed='8986300'
+          runningSince={null}
+          editFormOpen={false}
+        />
+        <EditableTimer
+          title='Learn extreme ironing'
+          project='World Domination'
+          elapsed='3890985'
+          runningSince={null}
+          editFormOpen
+        />
+      </div>
+    )
+  }
+}
+
+class EditableTimer extends React.Component {
+  render () {
+    if (this.props.editFormOpen) {
+      return (
+        <TimerForm
+          title={this.props.title}
+          project={this.props.project}
+        />
+      )
+    } else {
+      return (
+        <Timer
+          title={this.props.title}
+          project={this.props.project}
+          elapsed={this.props.elapsed}
+          runningSince={this.props.runningSince}
+        />
+      )
+    }
+  }
+}
+
+class TimerForm extends React.Component {
+  render () {
+    const submitText = this.props.title ? 'Update' : 'Create'
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='ui form'>
+            <div className='field'>
+              <label htmlFor=''>Title</label>
+              <input type='text' defaultValue={this.props.title} />
+            </div>
+            <div className='field'>
+              <label htmlFor=''>Project</label>
+              <input type='text' defaultValue={this.props.project} />
+            </div>
+            <div className='ui two bottom attached buttons'>
+              <button className='ui basic blue button'>{submitText}</button>
+              <button className='basic ui red button'>Cancel</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class ToggleableTimerForm extends React.Component {
+  render () {
+    if (this.props.isOpen) {
+      return (
+        <TimerForm />
+      )
+    } else {
+      return (
+        <div className='ui basic content center aligned element'>
+          <button className='ui basic button icon'>
+            <i className='plus icon' />
+          </button>
+        </div>
+      )
+    }
+  }
+}
+
+class Timer extends React.Component {
+  render () {
+    const elaspedString = helpers.renderElapsedString(this.props.elapsed)
+    return (
+      <div className='ui centered card'>
+        <div className='content'>
+          <div className='header'>{this.props.title}</div>
+          <div className='meta'>{this.props.object}</div>
+          <div className='center aligned description'>
+            <h2>{elaspedString}</h2>
+          </div>
+          <div className='extra content'>
+            <span className='right floated edit icon'>
+              <i className='edit icon' />
+            </span>
+            <span className='right floated trash icon'>
+              <i className='trash icon' />
+            </span>
+          </div>
+        </div>
+        <div className='ui bottom attached blue basic button'>
+          Start
+        </div>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <TimersDashboard />,
+  document.getElementById('content')
+)
--- fullstack-react-code/time_tracking_app/public/js/helpers.js
+++ fullstack-react-code/time_tracking_app/public/js/helpers.js
@@ -1,56 +1,56 @@
 window.helpers = (function () {
-  function newTimer(attrs = {}) {
+  function newTimer (attrs = {}) {
     const timer = {
       title: attrs.title || 'Timer',
       project: attrs.project || 'Project',
       id: uuid.v4(), // eslint-disable-line no-undef
-      elapsed: 0,
-    };
+      elapsed: 0
+    }
 
-    return timer;
+    return timer
   }
 
-  function findById(array, id, cb) {
+  function findById (array, id, cb) {
     array.forEach((el) => {
       if (el.id === id) {
-        cb(el);
-        return;
+        cb(el)
+        return
       }
-    });
+    })
   }
 
-  function renderElapsedString(elapsed, runningSince) {
-    let totalElapsed = elapsed;
+  function renderElapsedString (elapsed, runningSince) {
+    let totalElapsed = elapsed
     if (runningSince) {
-      totalElapsed += Date.now() - runningSince;
+      totalElapsed += Date.now() - runningSince
     }
-    return millisecondsToHuman(totalElapsed);
+    return millisecondsToHuman(totalElapsed)
   }
 
-  function millisecondsToHuman(ms) {
-    const seconds = Math.floor((ms / 1000) % 60);
-    const minutes = Math.floor((ms / 1000 / 60) % 60);
-    const hours = Math.floor(ms / 1000 / 60 / 60);
+  function millisecondsToHuman (ms) {
+    const seconds = Math.floor((ms / 1000) % 60)
+    const minutes = Math.floor((ms / 1000 / 60) % 60)
+    const hours = Math.floor(ms / 1000 / 60 / 60)
 
     const humanized = [
       pad(hours.toString(), 2),
       pad(minutes.toString(), 2),
-      pad(seconds.toString(), 2),
-    ].join(':');
+      pad(seconds.toString(), 2)
+    ].join(':')
 
-    return humanized;
+    return humanized
   }
 
-  function pad(numberString, size) {
-    let padded = numberString;
-    while (padded.length < size) padded = `0${padded}`;
-    return padded;
+  function pad (numberString, size) {
+    let padded = numberString
+    while (padded.length < size) padded = `0${padded}`
+    return padded
   }
 
   return {
     millisecondsToHuman,
     newTimer,
     findById,
-    renderElapsedString,
-  };
-}());
+    renderElapsedString
+  }
+}())
