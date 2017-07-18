bd083ed77cf8530d725eb3395a046eb8824f4149
Alex Griffis <alexgriffis@gmail.com>
2017-07-18 11:41:47 -0700

    Chapter 8: Testing with Jest

--- fullstack-react-code/testing/basics/Modash.test.js
+++ fullstack-react-code/testing/basics/Modash.test.js
@@ -1,35 +1,42 @@
-// We write the tests for the Modash library in
-// this file in the Unit Testing chapter
 import Modash from './Modash'
 
-function assertEqual(description, actual, expected) {
-  if (actual === expected) {
-    console.log(`[PASS] ${description}`)
-  } else {
-    console.error(`[FAIL] ${description}`)
-    console.error(`\tactual: '${actual}'`)
-    console.error(`\texpected: '${expected}'`)
-  }
-}
-let string = 'there was one catch, and that was CATCH-22'
-let actual = Modash.truncate(string, 19)
-let expected = 'there was one catch...'
-assertEqual('`truncate()`: truncates a string', actual, expected)
+describe('Modash', () => {
+  describe('`truncate()`', () => {
+    const string = 'there was one catch, and that was CATCH-22'
+    it('truncates a string', () => {
+      expect(
+        Modash.truncate(string, 19)
+      ).toEqual('there was one catch...')
+    })
 
-actual = Modash.truncate(string, string.length)
-expected = string
-assertEqual('`truncate()`: no-ops if <= length', actual, expected)
+    it('no-ops if <= length', () => {
+      expect(
+        Modash.truncate(string, string.length)
+      ).toEqual(string)
+    })
+  })
 
-actual = Modash.capitalize(string)
-expected = 'There was one catch, and that was catch-22'
-assertEqual('`capitalize()`: capitalizes the string', actual, expected)
-
-string = 'customer responded at'
-actual = Modash.camelCase(string)
-expected = 'customerRespondedAt'
-assertEqual('`camelCase()`: string with spaces', actual, expected)
-
-string = 'customer_responded_at'
-actual = Modash.camelCase(string)
-expected = 'customerRespondedAt'
-assertEqual('`camelCase()`: string with underscores', actual, expected)
+  describe('`capitalize()`', () => {
+    const string = 'there was one catch, and that was CATCH-22'
+    it('capitalizes the string', () => {
+      expect(
+        Modash.capitalize(string)
+      ).toEqual('There was one catch, and that was catch-22')
+    })
+  })
+  describe('`camelCase()`', () => {
+    const result = 'customerRespondedAt'
+    it('string with spaces', () => {
+      const spaces = 'customer responded at'
+      expect(
+        Modash.camelCase(spaces)
+      ).toEqual(result)
+    })
+    it('string with underscores', () => {
+      const underscores = 'customer_responded_at'
+      expect(
+        Modash.camelCase(underscores)
+      ).toEqual(result)
+    })
+  })
+})
