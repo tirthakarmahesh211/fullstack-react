57c64d770f297e29284782a75492f545ee3126ac
Alex Griffis <alexgriffis@gmail.com>
2017-07-20 10:41:36 -0700

    Chapter 8: Testing with Enzyme

--- fullstack-react-code/testing/react-basics/src/App.test.js
+++ fullstack-react-code/testing/react-basics/src/App.test.js
@@ -1,8 +1,113 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App'
+import React from 'react'
+import { shallow } from 'enzyme'
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-});
+describe('App', () => {
+  let wrapper
+  beforeEach(() => {
+    wrapper = shallow(
+      <App />
+    )
+  })
+  it('should have the `th` "Items"', () => {
+    expect(
+      wrapper.contains(<th>Items</th>)
+    ).toBe(true)
+  })
+
+  it('should have a `button` element', () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <button>Add item</button>
+      )
+    ).toBe(true)
+  })
+
+  it('`button` should be disabled', () => {
+    const button = wrapper.find('button').first()
+    expect(
+      button.props().disabled
+    ).toBe(true)
+  })
+
+  it('should have an `input` element', () => {
+    expect(
+      wrapper.containsMatchingElement(
+        <input />
+      )
+    ).toBe(true)
+  })
+  describe('the user populates the input', () => {
+    const item = 'Seattle'
+    beforeEach(() => {
+      const input = wrapper.find('input').first()
+      input.simulate('change', {
+        target: { value: item }
+      })
+    })
+
+    it('should update the state property `item`', () => {
+      expect(
+        wrapper.state().item
+      ).toEqual(item)
+    })
+
+    it('should enable `button`', () => {
+      const button = wrapper.find('button').first()
+      expect(
+        button.props().disabled
+      ).toBe(false)
+    })
+    describe('and then clears the input', () => {
+      beforeEach(() => {
+        const input = wrapper.find('input').first()
+        input.simulate('change', {
+          target: { value: '' }
+        })
+      })
+
+      it('should disable `button`', () => {
+        const button = wrapper.find('button').first()
+        expect(
+          button.props().disabled
+        ).toBe(true)
+      })
+    })
+
+    describe('and then clears the form', () => {
+      beforeEach(() => {
+        const form = wrapper.find('form').first()
+        form.simulate('submit', {
+          preventDefault: () => { }
+        })
+      })
+      it('should add the item to state.items', function () {
+        expect(
+          wrapper.state().items
+        ).toContain(item)
+      })
+
+      it('should render the new item to the table', function () {
+        expect(
+          wrapper.containsMatchingElement(
+            <td>{item}</td>
+          )
+        ).toBe(true)
+      })
+
+      it('should clear the `input` field', function () {
+        const input = wrapper.find('input').first()
+        expect(
+          input.props().value
+        ).toEqual('')
+      })
+
+      it('should disable `button`', function () {
+        const button = wrapper.find('button').first()
+        expect(
+          button.props().disabled
+        ).toBe(true)
+      })
+    })
+  })
+})
