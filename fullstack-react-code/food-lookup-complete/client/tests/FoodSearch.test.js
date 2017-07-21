1f3fad173d047e804e5d3b13dd29c9d5dc66b723
Alex Griffis <alexgriffis@gmail.com>
2017-07-21 16:54:43 -0700

    Chapter 8: Jest - Mocking an API for testing (1)

--- fullstack-react-code/food-lookup-complete/client/tests/FoodSearch.test.js
+++ fullstack-react-code/food-lookup-complete/client/tests/FoodSearch.test.js
@@ -1,47 +1,95 @@
 // We populate this file in the chapter "Unit Testing"
 /* eslint-disable no-unused-vars */
-import { shallow } from 'enzyme';
-import React from 'react';
-import FoodSearch from '../src/FoodSearch';
+import { shallow } from 'enzyme'
+import React from 'react'
+import FoodSearch from '../src/FoodSearch'
+import Client from '../src/Client'
+
+jest.mock('../src/Client')
 
 describe('FoodSearch', () => {
-  // ... initial state specs
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(
+      <FoodSearch />
+    )
+  })
+
+  afterEach(() => {
+    Client.search.mockClear()
+  })
+
+  it('should not display the remove icon', () => {
+    expect(
+      wrapper.find('.remove.icon').length
+    ).toBe(0)
+  })
+
+  it('should display zero rows', () => {
+    expect(
+      wrapper.find('tbody tr').length
+    ).toBe(0)
+  })
 
   describe('user populates search field', () => {
-    beforeEach(() => {
-      // ... simulate user typing "brocc" in input
-    });
+    const value = 'brocc'
 
-    // ... specs
+    beforeEach(() => {
+      const input = wrapper.find(`input`).first()
+      input.simulate('change', {
+        target: {
+          value
+        }
+      })
+    })
+
+    it('should update the state property `searchValue`', () => {
+      expect(
+        wrapper.state().searchValue
+      ).toEqual(value)
+    })
+
+    it('should display the remove icon', () => {
+      expect(
+        wrapper.find('.remove.icon').length
+      ).toBe(1)
+    })
+
+    it('should call `Client.search()` with `value`', () => {
+      const invocationArgs = Client.search.mock.calls[0]
+      expect(
+        invocationArgs[0]
+      ).toEqual(value)
+    })
 
     describe('and API returns results', () => {
       beforeEach(() => {
-        // ... simulate API returning results
-      });
 
+      })
       // ... specs
 
       describe('then user clicks food item', () => {
         beforeEach(() => {
           // ... simulate user clicking food item
-        });
+        })
 
         // ... specs
-      });
+      })
 
       describe('then user types more', () => {
         beforeEach(() => {
           // ... simulate user typing "x"
-        });
+        })
 
         describe('and API returns no results', () => {
           beforeEach(() => {
             // ... simulate API returning no results
-          });
+          })
 
           // ... specs
-        });
-      });
-    });
-  });
-});
+        })
+      })
+    })
+  })
+})
--- fullstack-react-code/testing/react-basics/src/App.test.js
+++ fullstack-react-code/testing/react-basics/src/App.test.js
@@ -1,3 +1,4 @@
+/* global describe, it, expect, beforeEach */
 import App from './App'
 import React from 'react'
 import { shallow } from 'enzyme'
