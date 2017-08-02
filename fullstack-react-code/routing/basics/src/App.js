5fc9e07bd54e564bb9ab9f1fd349c07a8cf2bfbe
Alex Griffis <alexgriffis@gmail.com>
2017-08-02 11:39:14 -0700

    Chapter 9: Roll your own Redirect

--- fullstack-react-code/routing/basics/src/App.js
+++ fullstack-react-code/routing/basics/src/App.js
@@ -29,6 +29,22 @@ Link.contextTypes = {
   history: PropTypes.object,
 }
 
+class Redirect extends React.Component {
+  static contextTypes = {
+    history: PropTypes.object,
+  }
+
+  componentDidMount() {
+    const history = this.context.history
+    const to = this.props.to
+    history.push(to)
+  }
+  render() {
+    return null
+  }
+}
+
+
 class Router extends React.Component {
   static childContextTypes = {
     history: PropTypes.object,
@@ -74,13 +90,18 @@ const App = () => (
             <code>/pacific</code>
           </Link>
         </li>
+        <li>
+          <Link to='/black-sea'>
+            <code>/black-sea</code>
+          </Link>
+        </li>
       </ul>
 
       <hr />
 
       <Route path='/atlantic' component={Atlantic} />
       <Route path='/pacific' component={Pacific} />
-
+      <Route path='/black-sea' component={BlackSea} />
     </div>
   </Router>
 )
@@ -104,5 +125,38 @@ const Pacific = () => (
     </p>
   </div>
 )
+
+class BlackSea extends React.Component {
+  state = {
+    counter: 3
+  }
+  componentDidMount() {
+    this.interval = setInterval(() => {
+      this.setState(prevState => {
+        return {
+          counter: prevState.counter - 1,
+        }
+      })
+    }, 1000)
+  }
+  componentWillUnmount() {
+    clearInterval(this.interval)
+  }
+  render() {
+    return (
+      <div>
+        <h3>Black Sea</h3>
+        <p>Nothing sea [sic] here...</p>
+        <p>Redirecting in {this.state.counter}...</p>
+        {
+          (this.state.counter < 1) ? (
+            <Redirect to='/' />
+          ) : null
+        }
+      </div>
+    )
+  }
+}
+
 
 export default App
