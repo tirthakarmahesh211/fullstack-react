063c167e694839f42958d59e12ba02c865a538bc
Alex Griffis <alexgriffis@gmail.com>
2017-08-14 11:49:12 -0700

    Chapter 10: Redux Basics (3)

--- fullstack-react-code/redux/chat_simple/src/App.js
+++ fullstack-react-code/redux/chat_simple/src/App.js
@@ -1,4 +1,4 @@
-
+import React, { Component } from 'react'
 
 function createStore(reducer, initialState) {
   let state = initialState
@@ -41,27 +41,86 @@ const initialState =  {
 }
 const store = createStore(reducer, initialState)
 
-const listener = () => {
-  console.log('Current state: ')
-  console.log(store.getState())
+class App extends Component {
+  componentDidMount () {
+    store.subscribe(() => this.forceUpdate())
+  }
+  
+  render() {
+    const messages = store.getState().messages
+    return (
+      <div className='ui segment'>
+        <MessageView messages={messages} />
+        <MessageInput />
+      </div>
+    )
+  }
 }
 
-store.subscribe(listener)
+class MessageInput extends Component {
+  state = {
+    value : ''
+  }
 
-const addMessageAction1 = {
-type: 'ADD_MESSAGE',
-message: 'How do you read?',
-};
-store.dispatch(addMessageAction1);
-// -> `listener()` is called
-const addMessageAction2 = {
-type: 'ADD_MESSAGE',
-message: 'I read you loud and clear, Houston.',
-};
-store.dispatch(addMessageAction2);
-// -> `listener()` is called
-const deleteMessageAction = {
-type: 'DELETE_MESSAGE',
-index: 0,
-};
-store.dispatch(deleteMessageAction);
+  onChange = (e) => {
+    this.setState({
+      value: e.target.value
+    })
+  }
+
+  handleSubmit = () => {
+    store.dispatch({
+      type: 'ADD_MESSAGE',
+      message: this.state.value,
+    })
+    this.setState({
+      value: ''
+    })
+  }
+
+  render () {
+    return (
+      <div className="ui input">
+        <input
+          onChange={this.onChange}  
+          value={this.state.value}
+          type='text'
+        />
+        <button
+          onClick={this.handleSubmit}
+          className='ui primary button'
+          type='submit'
+        >
+          Submit
+        </button>
+        </div>
+    )
+  }
+}
+
+class MessageView extends Component {
+  handleClick = (index) => {
+    store.dispatch({
+      type: 'DELETE_MESSAGE',
+      index: index,
+    })
+  }
+  render() {
+    const messages = this.props.messages.map((message, index) => (
+      <div
+        className='comment'
+        key={index}
+        onClick={() => this.handleClick(index)}
+      >
+        {message}
+      </div>  
+    ))
+    return (
+      <div className='ui comments'>
+        {messages}
+      </div>
+    )
+  }
+}
+
+export default App
