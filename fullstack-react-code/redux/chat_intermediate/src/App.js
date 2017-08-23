a2096f4a3947601ae5ec940185d5eed2b82e7045
Alex Griffis <alexgriffis@gmail.com>
2017-08-23 15:24:08 -0700

    Chapter 11:  Separating Presentation & Container Components

--- fullstack-react-code/redux/chat_intermediate/src/App.js
+++ fullstack-react-code/redux/chat_intermediate/src/App.js
@@ -103,33 +103,14 @@ function messagesReducer(state = [], action) {
 
 const store = createStore(reducer);
 
-class App extends React.Component {
-  componentDidMount() {
-    store.subscribe(() => this.forceUpdate());
-  }
+const App = () => (
+  <div className='ui segment'>
+    <ThreadTabs />
+    <ThreadDisplay />
+  </div>
+)
 
-  render() {
-    const state = store.getState()
-    const activeThreadId = state.activeThreadId
-    const threads = state.threads
-    const activeThread = threads.find((t) => t.id === activeThreadId);
-    const tabs = threads.map(({ title, id }) => (
-      { // a "tab" object
-        title,
-        id,
-        active: id === activeThreadId
-      }
-    ))
-    return (
-      <div className='ui segment'>
-        <ThreadTabs tabs={tabs} />
-        <Thread thread={activeThread} />
-      </div>
-    );
-  }
-}
-
-class MessageInput extends React.Component {
+class TextFieldSubmit extends React.Component {
   state = {
     value: '',
   };
@@ -141,11 +122,7 @@ class MessageInput extends React.Component {
   };
 
   handleSubmit = () => {
-    store.dispatch({
-      type: 'ADD_MESSAGE',
-      text: this.state.value,
-      threadId: this.props.threadId
-    });
+    this.props.onSubmit(this.state.value)
     this.setState({
       value: '',
     });
@@ -171,59 +148,117 @@ class MessageInput extends React.Component {
   }
 }
 
-class Thread extends React.Component {
-  handleClick = (id) => {
-    store.dispatch({
-      type: 'DELETE_MESSAGE',
-      id: id,
-    });
-  };
+
+const MessageList = (props) => {
+  return (
+    <div className='ui comments'>
+      {
+        props.messages.map((m, index) => (
+          <div
+            className='comment'
+            key={index}
+            onClick={() => props.onClick(m.id)}
+          >
+            <div className='text'>
+              {m.text}
+              <span className='metadata'>
+                @{m.timestamp}
+              </span>
+            </div>
+          </div>
+        ))
+      }
+    </div>
+  );
+};
+
+const Thread = props => {
+  return (
+    <div className='ui center aligned basic segment'>
+      <MessageList
+        messages={props.thread.messages}
+        onClick={props.onMessageClick}
+      />
+      <TextFieldSubmit
+        onSubmit={props.onMessageSubmit}
+      />
+    </div>
+  );
+};
+
+class ThreadDisplay extends React.Component {
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate())
+  }
 
   render() {
-    const messages = this.props.thread.messages.map((message, index) => (
-      <div
-        className='comment'
-        key={index}
-        onClick={() => this.handleClick(message.id)}
-      >
-        <div className='text'> {/*Wrap message data in `div` */}
-          {message.text}
-          <span className='metadata'>@{message.timestamp}</span>
-        </div>
-      </div>
-    ));
+    const state = store.getState()
+    const activeThreadId = state.activeThreadId
+    const activeThread = state.threads.find(
+      t => t.id === activeThreadId
+    )
     return (
-      <div className='ui center aligned basic segment'>
-        <div className='ui comments'>
-          {messages}
-        </div>
-        <MessageInput threadId={this.props.thread.id} />
-      </div>
-    );
+      <Thread
+        thread={activeThread}
+        onMessageClick={(id) => (
+          store.dispatch({
+            type: 'DELETE_MESSAGE',
+            id: id,
+          })
+        )}
+        onMessageSubmit={(text) => (
+          store.dispatch({
+            type: 'ADD_MESSAGE',
+            text,
+            threadId: activeThreadId,
+          })
+        )}
+      />
+    )
   }
 }
 
+const Tabs = (props) => (
+  <div className='ui top attached tabular menu'>
+    {
+      props.tabs.map((tab, index) => (
+        <div
+          key={index}
+          className={tab.active ? 'active item' : 'item'}
+          onClick={() => props.onClick(tab.id)}
+        >
+          {tab.title}
+        </div>
+      ))
+    }
+  </div>
+)
+
 class ThreadTabs extends Component {
-  handleClick = (id) => {
-    store.dispatch({
-      type: 'OPEN_THREAD',
-      id,
-    })
+  componentDidMount() {
+    store.subscribe(() => this.forceUpdate())
   }
+
   render() {
-    const tabs = this.props.tabs.map((tab, index) => (
-      <div
-        key={index}
-        className={tab.active ? 'active item' : 'item'}
-        onClick={() => this.handleClick(tab.id)}
-      >
-        {tab.title}
-      </div>
+    const state = store.getState()
+    const tabs = state.threads.map(t => (
+      {
+        title: t.title,
+        active: t.id === state.activeThreadId,
+        id: t.id,
+      }
     ))
+
     return (
-      <div className='ui top attached tabular menu'>
-        {tabs}
-      </div>
+      <Tabs
+        tabs={tabs}
+        onClick={(id) => (
+          store.dispatch({
+            type: 'OPEN_THREAD',
+            id,
+          })
+        )}
+      />
     )
   }
 }
