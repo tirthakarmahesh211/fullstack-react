f4fe1c22d390d79385e46e73be92b8c66d9b4b7a
Alex Griffis <alexgriffis@gmail.com>
2017-08-23 20:14:20 -0700

    Chapter 11: Generate containers with connect()

--- fullstack-react-code/redux/chat_intermediate/src/App.js
+++ fullstack-react-code/redux/chat_intermediate/src/App.js
@@ -1,6 +1,7 @@
 // @ts-check
 import React, { Component } from 'react';
 import { createStore, combineReducers } from 'redux'
+import { Provider, connect } from 'react-redux';
 import uuid from 'uuid'
 
 const reducer = combineReducers({
@@ -186,37 +187,44 @@ const Thread = props => {
   );
 };
 
-class ThreadDisplay extends React.Component {
-  componentDidMount() {
-    store.subscribe(() => this.forceUpdate())
+const mapStateToThreadProps = (state) => (
+  {
+    thread: state.threads.find(
+      t => t.id === state.activeThreadId
+    ),
   }
+)
+const mapDispatchToThreadProps = (dispatch) => (
+  {
+    onMessageClick: (id) => (
+      store.dispatch({
+        type: 'DELETE_MESSAGE',
+        id: id,
+      })
+    ),
+    dispatch,
+  }
+)
 
-  render() {
-    const state = store.getState()
-    const activeThreadId = state.activeThreadId
-    const activeThread = state.threads.find(
-      t => t.id === activeThreadId
-    )
-    return (
-      <Thread
-        thread={activeThread}
-        onMessageClick={(id) => (
-          store.dispatch({
-            type: 'DELETE_MESSAGE',
-            id: id,
-          })
-        )}
-        onMessageSubmit={(text) => (
-          store.dispatch({
-            type: 'ADD_MESSAGE',
-            text,
-            threadId: activeThreadId,
-          })
-        )}
-      />
-    )
+const mergeThreadProps = (stateProps, dispatchProps) => (
+  {
+    ...stateProps,
+    ...dispatchProps,
+    onMessageSubmit: (text) => (
+      dispatchProps.dispatch({
+        type: 'ADD_MESSAGE',
+        text,
+        threadId: stateProps.thread.id,
+      })
+    ),
   }
-}
+)
+
+const ThreadDisplay = connect(
+  mapStateToThreadProps,
+  mapDispatchToThreadProps,
+  mergeThreadProps
+)(Thread)
 
 const Tabs = (props) => (
   <div className='ui top attached tabular menu'>
@@ -234,34 +242,37 @@ const Tabs = (props) => (
   </div>
 )
 
-class ThreadTabs extends Component {
-  componentDidMount() {
-    store.subscribe(() => this.forceUpdate())
-  }
-
-  render() {
-    const state = store.getState()
-    const tabs = state.threads.map(t => (
-      {
-        title: t.title,
-        active: t.id === state.activeThreadId,
-        id: t.id,
-      }
-    ))
-
-    return (
-      <Tabs
-        tabs={tabs}
-        onClick={(id) => (
-          store.dispatch({
-            type: 'OPEN_THREAD',
-            id,
-          })
-        )}
-      />
-    )
+const mapStateToTabsProps = (state) => {
+  const tabs = state.threads.map(t => (
+    {
+      title: t.title,
+      active: t.id === state.activeThreadId,
+      id: t.id,
+    }
+  ))
+  return {
+    tabs,
   }
 }
 
+const mapDispatchToTabsProps = (dispatch) => (
+  {
+    onClick: (id) => (
+      store.dispatch({
+        type: 'OPEN_THREAD',
+        id,
+      })
+    )
+  }
+)
+const ThreadTabs = connect(
+  mapStateToTabsProps,
+  mapDispatchToTabsProps,
+)(Tabs)
 
-export default App;
+const WrappedApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+)
+export default WrappedApp;
