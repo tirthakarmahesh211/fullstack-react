a41db9c5bf50dc94acf481493c76f81df1826577
Alex Griffis <alexgriffis@gmail.com>
2017-08-31 17:45:12 -0700

    Chapter 12: GraphQL mutation

--- fullstack-react-code/graphql-server/server/server.js
+++ fullstack-react-code/graphql-server/server/server.js
@@ -5,7 +5,7 @@ import express from 'express';
 const app = express()
 
 import graphqlHTTP from 'express-graphql';
-import { GraphQLSchema, GraphQLObjectType, GraphQLString } from 'graphql';
+import { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLNonNull, GraphQLID } from 'graphql';
 
 const RootQuery = new GraphQLObjectType({
   name: 'RootQuery',
@@ -16,12 +16,47 @@ const RootQuery = new GraphQLObjectType({
       resolve() {
         return 'viewer!'
       }
+    },
+    node: {
+      type: GraphQLString,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID)
+        }
+      },
+      resolve(source, args) {
+        return inMemoryStore[args.key]
+      }
+    }
+  }
+})
+
+let inMemoryStore = {}
+const RootMutation = new GraphQLObjectType({
+  name: 'RootMutation',
+  description: 'The root mutation',
+  fields: {
+    setNode: {
+      type: GraphQLString,
+      args: {
+        id: {
+          type: new GraphQLNonNull(GraphQLID),
+        },
+        value: {
+          type: new GraphQLNonNull(GraphQLString)
+        }
+      },
+      resolve(source, args) {
+        inMemoryStore[args.key] = args.value
+        return inMemoryStore[args.key]
+      }
     }
   }
 })
 
 const Schema = new GraphQLSchema({
   query: RootQuery,
+  mutation: RootMutation,
 })
 
 app.use('/graphql', graphqlHTTP({ schema: Schema, graphiql: true }))
