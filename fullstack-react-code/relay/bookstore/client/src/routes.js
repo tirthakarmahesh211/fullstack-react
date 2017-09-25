316f870384c25ef6b25ceb0b776e66ffaeb00162
Alex Griffis <alexgriffis@gmail.com>
2017-09-25 13:10:06 -0700

    Chapter 13: App, Index & Routes Prep for Relay

--- fullstack-react-code/relay/bookstore/client/src/routes.js
+++ fullstack-react-code/relay/bookstore/client/src/routes.js
@@ -4,20 +4,7 @@ import IndexRoute from 'react-router/lib/IndexRoute';
 import Route from 'react-router/lib/Route';
 
 import App from './components/App';
-import BooksPage from './components/BooksPage';
-import BookPage from './components/BookPage';
 import AuthorPage from './components/AuthorPage';
-
-const ViewerQueries = {
-  viewer: () => Relay.QL`query { viewer }`,
-};
-
-const BookQueries = {
-  book: () => Relay.QL`
-  query { 
-    book(slug: $bookSlug)
-  }`,
-};
 
 const AuthorQueries = {
   author: () => Relay.QL`
@@ -31,15 +18,7 @@ export default (
     path='/'
     component={App}
   >
-    <IndexRoute
-      component={BooksPage}
-      queries={ViewerQueries}
-    />
-    <Route
-      path='/books/:bookSlug'
-      component={BookPage}
-      queries={BookQueries}
-    />
+
     <Route
       path='/authors/:authorId'
       component={AuthorPage}
